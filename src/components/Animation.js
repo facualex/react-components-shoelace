1ac720b8a94780d64378a6f83dfbf70613375da9
Facundo Alexandre <facualex@gmail.com>
2020-08-15 14:18:27 -0400

    Components linted

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,26 +1,22 @@
 import React from 'react'
-import wrapCustomElement from '@shoelace/react-wrapper';
 import styled from 'styled-components'
-import { compose, flexbox, space, color, layout, background } from 'styled-system'
+import {
+  compose,
+  flexbox,
+  space,
+  color,
+  layout,
+  background,
+} from 'styled-system'
 
-const ShoelaceAlert = wrapCustomElement('sl-alert');
+const ShoelaceAlert = wrapCustomElement(`sl-alert`)
 
 const Alert = styled(ShoelaceAlert)`
-    ${compose(
-        flexbox,
-        space,
-        color,
-        layout,
-        background,
-    )}
+  ${compose(flexbox, space, color, layout, background)}
 `
 
 function AlertWrapper({ children, ...props }) {
-    return (
-        <Alert>
-            {children}
-        </Alert>
-    )
+  return <Alert>{children}</Alert>
 }
 
 return AlertWrapper
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,26 +1,23 @@
 import React from 'react'
-import wrapCustomElement from '@shoelace/react-wrapper';
+import wrapCustomElement from '@shoelace-style-style/react-wrapper'
 import styled from 'styled-components'
-import { compose, flexbox, space, color, layout, background } from 'styled-system'
+import {
+  compose,
+  flexbox,
+  space,
+  color,
+  layout,
+  background,
+} from 'styled-system'
 
-const ShoelaceAlert = wrapCustomElement('sl-alert');
+const ShoelaceAlert = wrapCustomElement(`sl-alert`)
 
 const Alert = styled(ShoelaceAlert)`
-    ${compose(
-        flexbox,
-        space,
-        color,
-        layout,
-        background,
-    )}
+  ${compose(flexbox, space, color, layout, background)}
 `
 
 function AlertWrapper({ children, ...props }) {
-    return (
-        <Alert>
-            {children}
-        </Alert>
-    )
+  return <Alert>{children}</Alert>
 }
 
 return AlertWrapper
